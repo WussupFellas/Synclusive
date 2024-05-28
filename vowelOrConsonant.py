11db39edbdbd5d25b4de5779aeb3dc4283b65454
Hugo Dinis <hdinis09@gmail.com>
2024-05-28 15:06:54 +0100

    Version 1.0

---
+++ vowelOrConsonant.py
@@ -0,0 +1,8 @@
+x = str(input("Enter a letter: "))
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+if x in vowels:
+    print(x, " is a vowel.")
+else:
+    print(x, "is a consonant.")
+
