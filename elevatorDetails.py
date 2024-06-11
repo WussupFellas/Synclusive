3233382cf0ae57261837aa3ec658bee76cefb332
Hugo Dinis <hdinis09@gmail.com>
2024-06-11 15:23:09 +0100

    Version 1.0

---
+++ elevatorDetails.py
@@ -0,0 +1,7 @@
+class Elevator:
+    def __init__(self, floor, max_floor, cap, max_cap):
+        self.floor = floor
+        self.max_floor = max_floor
+        self.cap = cap
+        self.max_cap = max_cap
+        
